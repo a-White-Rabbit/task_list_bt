7f46eabbdf3a09bb374ea4b5e089cce124c2a773
Marc E Auciello <you@example.com>
2022-10-03 07:33:56 -0400

    delete & filter funk

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ loadEventListeners();
 
 function loadEventListeners() {
    form.addEventListener('submit', addTask);
+   taskList.addEventListener('click', removeTask);
+   clearBtn.addEventListener('click', clearTasks);
+   filter.addEventListener('keyup', filterTasks);  
 }
 
 function addTask(e) { 
@@ -29,3 +32,34 @@ function addTask(e) {
    }
    e.preventDefault(); 
 }
+
+function removeTask(e) {
+   if(e.target.parentElement.classList.contains('delete-item')) {
+      if(confirm('Are you sure?')) {
+         e.target.parentElement.parentElement.remove();
+      }
+   }
+}
+
+function clearTasks(e) {
+   while(taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
+   }
+}
+
+function filterTasks(e) {
+   const text = e.target.value.toLowerCase();
+   const collection = document.querySelectorAll('.collection-item')
+   collection.forEach(function(task) {
+      const item = task.firstChild.textContent;
+      if(item.toLowerCase().indexOf(text) != -1) {
+         task.style.display = 'block';
+      } else {
+         task.style.display = 'none';
+      }
+   });
+
+}
+
+
+
